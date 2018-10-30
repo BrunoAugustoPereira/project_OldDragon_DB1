d8baab1e445f70e070fdfadc0c667923877d6870
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2018-10-30 12:12:29 -0300

    Add files via upload
    script to populate the database.
    populated the jogador table

---
+++ scriptPopular.sql
@@ -0,0 +1,39 @@
+INSERT INTO public.jogador(
+	id, nome)
+	VALUES (1,'Bruno Augusto');
+
+INSERT INTO public.jogador(
+	id, nome)
+	VALUES (2,'Miguel Araujo');
+	
+INSERT INTO public.jogador(
+	id, nome)
+	VALUES (3,'Davi Bras');
+	
+INSERT INTO public.jogador(
+	id, nome)
+	VALUES (4,'Arthur Barroso');
+	
+INSERT INTO public.jogador(
+	id, nome)
+	VALUES (5,'Pedro Simiao');
+	
+INSERT INTO public.jogador(
+	id, nome)
+	VALUES (6,'Gabriel Oliveira');
+	
+INSERT INTO public.jogador(
+	id, nome)
+	VALUES (7,'Sophia Marques');
+	
+INSERT INTO public.jogador(
+	id, nome)
+	VALUES (8,'Alice Pontes');
+	
+INSERT INTO public.jogador(
+	id, nome)
+	VALUES (9,'Julia Vieira');
+	
+INSERT INTO public.jogador(
+	id, nome)
+	VALUES (10,'Isabella Rabelo');
