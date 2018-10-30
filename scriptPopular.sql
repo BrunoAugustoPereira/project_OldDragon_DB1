1083cfb90588cfa3539c6cf31e75ffffda6e40cb
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2018-10-30 13:01:31 -0300

    populated the mestre,classe,idioma and raca tables

--- scriptPopular.sql
+++ scriptPopular.sql
@@ -1,3 +1,5 @@
+-- populate the jogador table
+
 INSERT INTO public.jogador(
 	id, nome)
 	VALUES (1,'Bruno Augusto');
@@ -37,3 +39,114 @@ INSERT INTO public.jogador(
 INSERT INTO public.jogador(
 	id, nome)
 	VALUES (10,'Isabella Rabelo');
+
+	
+--populate the mestre table	
+
+INSERT INTO public.mestre(
+	id, nome)
+	VALUES (1, 'Dimitri Gadelha');
+
+INSERT INTO public.mestre(
+	id, nome)
+	VALUES (2, 'Bruno Melgaco');
+
+INSERT INTO public.mestre(
+	id, nome)
+	VALUES (3, 'Lius Palacio');
+	
+INSERT INTO public.mestre(
+	id, nome)
+	VALUES (4, 'Lyana Stark');
+	
+INSERT INTO public.mestre(
+	id, nome)
+	VALUES (5, 'Dino Thomas');
+
+INSERT INTO public.mestre(
+	id, nome)
+	VALUES (6, 'Alex Hunter');
+	
+INSERT INTO public.mestre(
+	id, nome)
+	VALUES (7, 'Saphira Dragon');
+	
+INSERT INTO public.mestre(
+	id, nome)
+	VALUES (8, 'Luna Lovegood');
+
+-- populate the classe table	
+	
+INSERT INTO public.classe(
+	id, nome, dado_de_vida)
+	VALUES (1, 'clerigo', 'd8');
+	
+INSERT INTO public.classe(
+	id, nome, dado_de_vida)
+	VALUES (2, 'homem de armas', 'd10');
+	
+INSERT INTO public.classe(
+	id, nome, dado_de_vida)
+	VALUES (3, 'ladrao', 'd6');
+	
+INSERT INTO public.classe(
+	id, nome, dado_de_vida)
+	VALUES (4, 'mago', 'd4');
+	
+-- populate the idioma table	
+	
+INSERT INTO public.idioma(
+	id, nome)
+	VALUES (1, 'Comum');
+
+INSERT INTO public.idioma(
+	id, nome)
+	VALUES (2,'Anao');
+	
+INSERT INTO public.idioma(
+	id, nome)
+	VALUES (3, 'Elfico');
+	
+INSERT INTO public.idioma(
+	id, nome)
+	VALUES (4,'Silvestre');
+	
+INSERT INTO public.idioma(
+	id, nome)
+	VALUES (5, 'Goblinoide');
+	
+INSERT INTO public.idioma(
+	id, nome)
+	VALUES (6, 'Orc');
+	
+INSERT INTO public.idioma(
+	id, nome)
+	VALUES (7, 'Gigante');
+	
+INSERT INTO public.idioma(
+	id, nome)
+	VALUES (8, 'Draconico');
+	
+INSERT INTO public.idioma(
+	id, nome)
+	VALUES (9, 'Antigo');
+	
+-- populate the raca table	
+
+INSERT INTO public.raca(
+	id, nome, tamanho_usual, idade_maturidade, expectativa_de_vida, movimentacao_base, id_idioma)
+	VALUES (1, 'Humano', 175, 15, 70, 9, 1);
+	
+INSERT INTO public.raca(
+	id, nome, tamanho_usual, idade_maturidade, expectativa_de_vida, movimentacao_base, id_idioma)
+	VALUES (2, 'Anao', 140, 70, 350, 6, 2);
+	
+INSERT INTO public.raca(
+	id, nome, tamanho_usual, idade_maturidade, expectativa_de_vida, movimentacao_base, id_idioma)
+	VALUES (3, 'Elfo', 160, 150, 700, 9, 3);
+	
+INSERT INTO public.raca(
+	id, nome, tamanho_usual, idade_maturidade, expectativa_de_vida, movimentacao_base, id_idioma)
+	VALUES (4, 'Halfling', 80, 30, 70, 6, 1);
+	
+	
