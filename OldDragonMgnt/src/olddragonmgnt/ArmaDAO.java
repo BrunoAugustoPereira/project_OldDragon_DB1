f917ea32dba5d880aae8d17352ce5d4d78903a7d
BrunoAugusto <bruno.augusto.pp@gmail.com>
2018-11-12 23:15:23 -0300

    add function insert new weapon

--- OldDragonMgnt/src/olddragonmgnt/ArmaDAO.java
+++ OldDragonMgnt/src/olddragonmgnt/ArmaDAO.java
@@ -6,6 +6,9 @@
 package olddragonmgnt;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 
 
@@ -16,11 +19,39 @@ import java.sql.Connection;
 public class ArmaDAO {
     public static String URL= "jdbc:postgresql://localhost:5432/OD";
     public static String USUARIO= "postgres";
-    public static String SENHA= "ufc123";
+    public static String SENHA= "password";
     Connection c = null;
     
-    public void insertNewWeapon(){
-    
+    public void insertNewWeapon(String nome, String tipoDeDano, String tamanho, String alcance, int preco , String dano, int peso, String chanceDeCritico ) throws SQLException{
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection(URL, USUARIO, SENHA); String sql = "INSERT INTO public.arma(nome, tipo_de_dano, tamanho, alcance, preco, dano, peso, chance_de_critico) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+            PreparedStatement stmt = c.prepareStatement(sql);
+            stmt.setString(1,nome);
+            stmt.setString(2,tipoDeDano);
+            stmt.setString(3,tamanho);
+            stmt.setString(4,alcance);
+            stmt.setInt(5,preco);
+            stmt.setString(6,dano);
+            stmt.setInt(7,peso);
+            stmt.setString(8,chanceDeCritico);
+
+            int resultado = stmt.executeUpdate();
+            if (resultado == 1) {
+            System.out.println("arma inserida com sucesso");
+            } else {
+            System.out.println("Não foi possível inserir esta arma");
+            }
+            stmt.close();
+            c.close();
+        } 
+        catch (ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } 
+        catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+   
 
     }
 }
